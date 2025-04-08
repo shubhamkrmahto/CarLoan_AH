5d628844088c95151836c61cf6e2e868b375e6ed
shubhamkrmahto <skm9258@gmail.com>
2025-04-08 14:46:57 +0530

    Created all the layers for LoanDisbursement

---
+++ CLMS-AH-API-Module/src/main/java/com/app/ClmsAhApiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClmsAhApiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClmsAhApiModuleApplication.class, args);
+	}
+
+}
---
+++ CLMS-AH-API-Module/src/main/java/com/app/model/LoanDisbursement.java
@@ -0,0 +1,36 @@
+package com.app.model;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.Cascade;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanDisbursement {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer	agreementId;
+	private Integer loanNo;
+	private LocalDate agreementDate;
+	private String amountPayType;
+	private Double totalAmount;
+	private String bankName;
+	private Long accountNumber;
+	private String IFSCCode;
+	private String accountType;
+	private Double transferAmount;
+	private String paymentStatus;
+	private LocalDate amountPaidDate;
+
+}
